0c0774bdd9686dd4f5ae6e424222c33fae0d1bf4
Yu <Yu@10.208.79.84>
2014-07-17 17:03:23 +0900

    onClose handler initiated.

--- src/hurricane/client/HurricaneClientSocket.java
+++ src/hurricane/client/HurricaneClientSocket.java
@@ -9,11 +9,13 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.StatusCode;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
-/**
+/**WebSocket object on client side. Defined by annotations.
  * @author Romancer
  *
  */
@@ -28,6 +30,10 @@ public class HurricaneClientSocket {
 		this.nickname = nickname;
 	}
 
+	/**Handler of onConnect event. Run the first input waiting loop and accept a stdin input as destination username.<br>
+	 * Then send it to the server by "u" method. 
+	 * @param session
+	 */
 	@OnWebSocketConnect
 	public void onConnect(Session session) {
 		System.out.printf("Connected: %s\n", session);
@@ -43,31 +49,36 @@ public class HurricaneClientSocket {
 				fut = this.session.getRemote().sendStringByFuture(HurricaneMessage.SOCK_USER + stdin);
 				fut.get(100, TimeUnit.MILLISECONDS);
 			}
-//			fut = this.session.getRemote().sendStringByFuture(HurricaneMessage.SOCK_USER + this.nickname);
-//			fut.get(2, TimeUnit.SECONDS);
-//			if (!fut.isDone()) {
-//				if (this.session.isOpen()) this.session.close(StatusCode.SERVER_ERROR, "Server does not respond.");
-//			}
-//			else {
-//				//successfully sent target name
-//				
-//				//sending "Hurricane 99999999" to server as an example.
-//				fut = this.session.getRemote().sendStringByFuture(this.inputToTextMessage("Hurricane 99999999"));
-//				fut.get(2, TimeUnit.SECONDS);
-//			
-//				this.session.close(StatusCode.NORMAL, "Done.");
-//			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-//			try {
-//				if (this.session.isOpen()) this.session.close(StatusCode.SHUTDOWN, "Client shutdown.");
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
 		}
 	}
 	
+	/**Handler of onClose event. This event could be thrown in one of two ways: closed by server, or closed by client.<br>
+	 * If the session was closed by the server, there must be StatusCode or Reason got passed, read them and react appropriately.<br>
+	 * If closed by the client, same treatment, but you may prompt the socket or the client as you wish (no need to retry etc.)<br>
+	 * @param session
+	 */
+	@OnWebSocketClose
+	public void onClose(Session session, int statusCode, String reason) {
+		switch(statusCode) {
+		case StatusCode.NORMAL :
+			//to be implemented
+			return;
+		case StatusCode.SERVER_ERROR :
+			//to be implemented
+			return;
+		default :
+			//to be implemented
+			return;
+		}
+	}
+	
+	/**Primary text transaction should be defined here.<br>
+	 * Hurricane transactions are done by "methods", which are defined in HurricaneMessage.
+	 * @param session
+	 * @param text
+	 */
 	@OnWebSocketMessage
 	public void onMessage(Session session, String text) {
 		System.out.println();
