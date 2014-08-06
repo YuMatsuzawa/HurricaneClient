a1f18cfbffbcb77002ea76834e6202b730021728
Romancer <celestial.rhapsody.0824@gmail.com>
2014-08-06 21:25:51 +0900

    web client touched a bit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 //var ws = new WebSocket("ws://localhost:8080/");
-var ws = new WebSocket("ws://localhost:8080/hurricane");
+var protocol = "ws://";
+var host = "localhost";
+var urlDelim = ":";
+var listenPort = "8080";
+var contextPath = "/hurricane/server/jetty/primary";
+//var ws = new WebSocket("ws://localhost:8080/hurricane");
+var clientSock = new WebSocket(protocol + host + urlDelim + listenPort + contextPath);
 
 ws.onopen = function() {
     alert("Opened!");
